86173ce5a112c2b42f3fdccfc79fd4f3bb7b7d74
CSJ-094 <niznia7852@gmail.com>
2025-10-13 15:58:09 +0900

    re4
    dao dto등 필요파일 추가

--- Product_sales_platform/src/main/java/Product_sales_platform/dto/SelDTO.java
+++ Product_sales_platform/src/main/java/Product_sales_platform/dto/SelDTO.java
@@ -1,5 +1,18 @@
 package Product_sales_platform.dto;
 
-public class SelDTO {
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SelDTO {
+	private String MEMBER_ID;
+	private String MEMBER_PW;
+	private String MEMBER_NAME;
+	private String MEMBER_EMAIL;
+	private String MEMBER_PHONE;
+	private String MEMBER_ADDR;
 }
